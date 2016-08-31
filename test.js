2f940c4547b61eeba4a5957f4a0ab576a413ae12
Matthew Clark <matt@notch8.com>
2016-08-31 09:46:48 -0700

    adds ability to move keeper around the board

--- test.js
+++ test.js
@@ -4,17 +4,97 @@ var prompt = require('prompt');
 
 prompt.start();
 
-prompt.get('board', function (err, result) {
+prompt.get('board', function(err, result) {
   if (err) { return onErr(err); }
  
   var boardComponents = parseInput(result.board)
-  displayBoard(boardComponents);
-
-  //console.log(result.board.replace(/\|/g,'\n').replace(/-/g,' '));
+  playGame(boardComponents);
+  
 });
 
+function playGame(boardComponents){
+  if(isWon(boardComponents)){
+    console.log("You win!");
+    return;
+  };
+
+  displayBoard(boardComponents);
+  prompt.get('move', function(err, result){
+    playGame(getNextBoard(boardComponents, result.move));
+  });
+};
+
+function keeperBoardPosition(boardComponents){
+  var position = {};
+  boardComponents.forEach(function(row, rowIndex){
+    row.forEach(function(column, columnIndex){
+      if(_.includes(["@","+"], column)){
+        position.rowIndex = rowIndex;
+        position.columnIndex = columnIndex;
+      }
+    });
+  });
+  return position;
+};
+
+function newPosition(coordinates, move){
+  var rowIndex = coordinates.rowIndex;
+  var columnIndex = coordinates.columnIndex;
+  switch(move){
+    case 'up':
+      return {rowIndex: rowIndex - 1, columnIndex: columnIndex};
+    case 'down':
+      return {rowIndex: rowIndex + 1, columnIndex: columnIndex};
+    case 'left':
+      return {rowIndex: rowIndex, columnIndex: columnIndex - 1};
+    case 'right':
+      return {rowIndex: rowIndex, columnIndex: columnIndex + 1};
+  };
+};
+
+function positionValue(boardComponents, rowIndex, columnIndex){
+  return boardComponents[rowIndex][columnIndex];
+};
+
+function isValidMove(boardComponents, coordinates){
+  var rowIndex = coordinates.rowIndex;
+  var columnIndex = coordinates.columnIndex;
+  return positionValue(boardComponents, rowIndex, columnIndex) != '#';
+};
+
+function updateBoard(boardComponents, keeperPosition, targetPosition){
+  boardComponents[keeperPosition.rowIndex][keeperPosition.columnIndex] = '-';
+  boardComponents[targetPosition.rowIndex][targetPosition.columnIndex] = '@';
+  return boardComponents;
+};
+
+function getNextBoard(boardComponents, move){
+  var keeperPosition = keeperBoardPosition(boardComponents);
+  var targetPosition = newPosition(keeperPosition, move);
+  if(!isValidMove(boardComponents, targetPosition)){
+    return boardComponents;
+  };
+
+  boardComponents = updateBoard(boardComponents, keeperPosition, targetPosition)
+
+  //switch(move){
+  //  case 'up':
+  //    moveUp(boardComponents)
+  //  case 'down':
+  //  case 'left':
+  //  case 'right':
+  //  default :
+  //    console.log('invalid move');
+  //    return boardComponents;
+  //};
+  return boardComponents;
+};
+
+function isWon(boardComponents){
+  return !_.includes(_.flattenDeep(boardComponents), '$');
+};
+
 // #|#.@-#-#|#$*-$-#|#3-$-#|#-..--#|#--*--#|7#
-// #|#.@-#-#|#$*-$-#|#3-$-#|#-..--#|#--*--#|7#8
 function parseInput(input){
   var boardRows = input.split("|")
   var boardComponents = boardRows.map(function(row){ 
@@ -31,7 +111,6 @@ function parseInput(input){
       }
     }
 
-    console.log(rowPositions);
     return rowPositions;
   });
   return boardComponents;
